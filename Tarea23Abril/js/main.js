bd73c2fdfc029505d11ccd17cc4ca800cba90c05
Alejandro Torres <torresalexatlanta@gmail.com>
2024-04-24 14:08:52 -0600

    TareaAbril23

--- Tarea23Abril/js/main.js
+++ Tarea23Abril/js/main.js
@@ -1,12 +1,41 @@
-let selectedCheckbox = document.querySelectorAll(".checkbox")
+/*
+  1.- Activar el formulario, de tal forma que al dar click en el botón "crear usuario" se genere un objeto con la siguiente forma:
+  person = {
+    name:"...",
+    email:"...",
+    password:"..."
+  }
+  2.- Agregar a la funcionalidad, una lista "persons", de tal forma que al dar click en el boton, la persona creada en el paso anterior, se agregue a esa lista "persons"
 
-let getSelectBox = () => {
-    selectedCheckbox.forEach((checkbox) => {
-        checkbox.addEventListener("change", () =>{
-            let caja = document.querySelector(checkbox.getAttribute("data-target"))
-            checkbox.checked ? caja.classList.add("d-none") : caja.classList.replace("d-none", "d-block") 
+   persons = []
+  person -> persons
+
+  3.- Agregar la funcionalidad para que cada que agregue una persona nueva, esta se vea reflejada junto con las anteriores en la interfaz de usuario
+  */
+
+let personInformation = document.querySelectorAll(".info-person")
+let buttonPersonInfo = document.getElementById("createPerson")
+let displayedPersonsList = document.getElementById("listPersons")
+let personList = []
+
+let getPersonInfo = () => {
+
+    buttonPersonInfo.addEventListener("click", (event) =>{
+        event.preventDefault()
+        let personObj = {}
+        personInformation.forEach((person) => {
+          personObj[person.name] = person.value;
+          person.value = ""
+        })
+        personList = [...personList, personObj]
+        displayedPersonsList.innerHTML = ""; 
+        personList.forEach((person) => {
+          let li = document.createElement('li')
+          li.innerHTML = `FullName : ${person.FullName}, Email : ${person.Email}, PassWord : ${person.Password}`
+          displayedPersonsList.appendChild(li)
         })
     })
 }
 
-getSelectBox()
+
+getPersonInfo()
